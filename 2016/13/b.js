d5d917ac82f09b3852c59579aa5cb1d6f234d24b
Jamie Humphries <j@miehumphri.es>
2016-12-13 09:27:31 +0000

    y2016 q13a

---
+++ 2016/13/b.js
@@ -0,0 +1,22 @@
+var n = 1358;
+
+wall = (x, y) => {
+  var num = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + n;
+  var bin = num.toString(2);
+  return bin.split('').filter(x => parseInt(x)).length % 2 === 1;
+}
+
+var size = 50;
+for (var y = 0; y < size; y++) {
+  var row = '';
+  for (var x = 0; x < size; x++) {
+    if (x === 1 && y === 1) {
+      row += 'X';
+    } else {
+      row += wall(x, y) ? '█' : '•';
+    }
+  }
+  console.log(row);
+}
+
+// Manually count!
