456095ed7c0cc6521beab7cf5a14850c06ed9785
Jamie Humphries <j@miehumphri.es>
2016-12-27 11:23:35 +0000

    y2016 q22b

--- 2016/22/b.js
+++ 2016/22/b.js
@@ -6,42 +6,35 @@ viable = (a, b) => {
   return a.used !== 0 && b.avail >= a.used;
 }
 
-state = (grid, goalX, goalY) => `${[].concat(...grid).map(node => node.avail).join()}:${goalX},${goalY}`;
-
 run = input => {
   var lines = input.split('\n');
-  lines.splice(0, 2); // Remove junk lines.
-  const NODE_PATTERN = /\/dev\/grid\/node-x(\d+)-y(\d+)\s+\d+T\s+(\d+)T\s+(\d+)T\s+\d+%/
+  const NODE_PATTERN = /\/dev\/grid\/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)%/
   nodes = lines.map(line => {
     var match = line.match(NODE_PATTERN);
     return {
       x: parseInt(match[1]),
       y: parseInt(match[2]),
-      used: parseInt(match[3]),
-      avail: parseInt(match[4])
+      size: parseInt(match[3]),
+      used: parseInt(match[4]),
+      avail: parseInt(match[5]),
+      use: parseInt(match[6])
     }
   });
 
-  var maxX = 29;
-  var maxY = 34;
-  var grid = new Array(maxX + 1).fill(null).map(() => new Array(maxY + 1).fill(null));
-  nodes.forEach(node => grid[node.x][node.y] = { used: node.used, avail: node.avail });
+  var maxX = Math.max(...nodes.map(n => n.x));
+  var maxY = Math.max(...nodes.map(n => n.y));
 
-  return state(grid, 5, 3);   
+  var grid = new Array(maxY + 1).fill().map(() => new Array(maxX + 1).fill(0));
+  nodes.forEach(node => {
+    var symbol = node.used === 0 ? ' ' : node.used < 100 ? '•' : '█';
+    grid[node.y][node.x] = symbol;
+  });
+  grid[0][maxX] = 'G';
 
-  var viablePairs = [];
-  for (var a = 0; a < nodes.length; a++) {
-    for (var b = a + 1; b < nodes.length; b++) {
-      var nodeA = nodes[a];
-      var nodeB = nodes[b];
-      if (viable(nodeA, nodeB)) {
-        viablePairs++;
-      }
-      if (viable(nodeB, nodeA)) {
-        viablePairs++;
-      }
-    }
-  }
+  var cols = new Array(maxX + 1).fill().map((_, i) => (i < 10 ? '0' + i : i));
+  console.log('   ' + cols.map(c => Math.floor(c / 10)).join(' '));
+  console.log('   ' + cols.map(c => c % 10).join(' '));
+  grid.forEach((row, i) => console.log((i < 10 ? '0' + i : i) + ' ' + row.join(' ')));
 
-  return viablePairs;
+  // Manually calculate.
 }
