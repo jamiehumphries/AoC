a97d36701ef752e6e770e508a960b9aa59d80487
Jamie Humphries <j@miehumphri.es>
2018-11-30 10:28:59 +0000

    Second part of question 10.

--- 2017/q10.js
+++ 2017/q10.js
@@ -1,7 +1,6 @@
-const input = `230,1,2,221,97,252,168,169,57,99,0,254,181,255,235,167`;
+const input = '230,1,2,221,97,252,168,169,57,99,0,254,181,255,235,167';
 const listLength = 256;
 
-const lengths = input.split(',').map(n => parseInt(n));
 let list = [...Array(listLength).keys()]
 
 let currentPosition = 0;
@@ -20,11 +19,32 @@ function reverse(length) {
   list = uncircleTail.concat(list);
 }
 
-lengths.forEach(length => {
-  reverse(length);
-  currentPosition = (currentPosition + length + skipSize) % listLength;
-  skipSize++;
-});
+// const lengths = input.split(',').map(n => parseInt(n));
+const lengths = input.split('').map(char => char.charCodeAt()).concat([17, 31, 73, 47, 23]);
+const rounds = 64;
+for (let round = 1; round <= rounds; round++) {
+  lengths.forEach(length => {
+    reverse(length);
+    currentPosition = (currentPosition + length + skipSize) % listLength;
+    skipSize++;
+  });
+}
+
+function denseHash(spareHash) {
+  let hash = '';
+  for (let block = 0; block * 16 < spareHash.length; block++) {
+    let blockHashNumber = 0;
+    for (let i = 0; i < 16; i ++) {
+      blockHashNumber ^= spareHash[block * 16 + i];
+    }
+    let blockHash = blockHashNumber.toString(16);
+    blockHash = blockHash.length === 1 ? '0' + blockHash : blockHash;
+    hash += blockHash;
+  }
+  return hash;
+}
 
 const result1 = list[0] * list[1];
 result1;
+const result2 = denseHash(list);
+result2;
